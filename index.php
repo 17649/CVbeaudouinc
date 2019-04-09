82f425597a193bbe81572e90e33eb2fe1a8f55ad
Beaudouin Camille <beaudouin.camille@outlook.fr>
2019-04-09 16:55:37 +0200

    Premiers onglets 2

--- index.php
+++ index.php
@@ -5,38 +5,27 @@
         die;
     }
 
+    $menu = [
+        [urlencode("expériences"), "Expériences", "Mes expériences"],
+        [urlencode("compétences"), "Compétences", "Mes compétences"],
+        [urlencode("formation"), "Formation", "Ma formation"],
+        [urlencode("hobbies"), "Hobbies et intêrets", "Mes hobbies et intêrets"],
+        [urlencode("contact"), "Contactez-moi", "Me contacter"]
+    ];
+
     $year = date_diff(date_create($data["BORN"]), new DateTime())->y;
 
+    $sectionName = "";
     if (isset($_GET['page']) && !empty($_GET['page'])) {
-        switch ($_GET['page']) {
-            case 'expériences':
-                $sectionName = "Mes expériences";
-                break;
-            case "compétences":
-                $sectionName = "Mes compétences";
-                break;
-            case "formation":
-                $sectionName = "Ma formation";
-                break;
-            case "hobbies":
-                $sectionName = "Mes hobbies et intérêts";
-                break;
-            case "contact":
-                $sectionName = "Me contacter";
-                break;
-            default:
-                $sectionName = "";
+        foreach ($menu as $m) {
+            if (urldecode($_GET['page']) === urldecode($m[0])) {
+                $sectionName = $m[2];
+            }
         }
     }
+?>
 
-$menu = [
-    [urlencode("expériences"), "Expériences", "Mes expériences"],
-    [urlencode("compétences"), "Compétences", "Mes compétences"],
-    [urlencode("formation"), "Formation", "Ma formation"],
-    [urlencode("hobbies"), "Hobbies et intêrets", "Mes hobbies et intêrets"],
-    [urlencode("contact"), "Contactez-moi", "Me contacter"]
-];
-
+<html lang="fr">
 <head>
     <meta charset="utf-8">
     <title>CV en ligne</title>
@@ -45,21 +34,19 @@ $menu = [
 <body>
 <header>
     <nav>
-        <a href="#">Expériences</a>
-        <a href="#">Compétences</a>
-        <a href="#">Formation</a>
-        <a href="#">Hobbies et intêrets</a>
-        <a href="#">Contactez moi</a>
+        <?php foreach ($menu as $m): ?>
+        <a href="?page=<?= $m[0]; ?>"><?= $m[1]; ?></a>
+        <?php endforeach; ?>
     </nav>
 </header>
 <main>
     <div class="title title-last-name"><?= $data['LAST_NAME']; ?></div>
     <div class="title title-first-name"><?= $data['FIRST_NAME']; ?></div>
-    <div class="title"><?= $sectionName ?></div>
     <div class="description">
         <p><?= $year; ?> ans // <?= $data['PHONE_NUMBER']; ?> // <?= $data['MAIL']; ?> //</p>
         <p><?= $data['ADDRESS']; ?></p>
     </div>
+    <div class="section-title"><?= $sectionName ?></div>
 </main>
 </body>
 </html>
