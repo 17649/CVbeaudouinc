dc526f24e7d37a287c80c01e32cb064b163b1c92
Beaudouin Camille <beaudouin.camille@outlook.fr>
2019-04-09 16:46:10 +0200

    Premiers onglets

--- index.php
+++ index.php
@@ -6,9 +6,37 @@
     }
 
     $year = date_diff(date_create($data["BORN"]), new DateTime())->y;
-?>
-<!doctype html>
-<html lang="fr">
+
+    if (isset($_GET['page']) && !empty($_GET['page'])) {
+        switch ($_GET['page']) {
+            case 'expériences':
+                $sectionName = "Mes expériences";
+                break;
+            case "compétences":
+                $sectionName = "Mes compétences";
+                break;
+            case "formation":
+                $sectionName = "Ma formation";
+                break;
+            case "hobbies":
+                $sectionName = "Mes hobbies et intérêts";
+                break;
+            case "contact":
+                $sectionName = "Me contacter";
+                break;
+            default:
+                $sectionName = "";
+        }
+    }
+
+$menu = [
+    [urlencode("expériences"), "Expériences", "Mes expériences"],
+    [urlencode("compétences"), "Compétences", "Mes compétences"],
+    [urlencode("formation"), "Formation", "Ma formation"],
+    [urlencode("hobbies"), "Hobbies et intêrets", "Mes hobbies et intêrets"],
+    [urlencode("contact"), "Contactez-moi", "Me contacter"]
+];
+
 <head>
     <meta charset="utf-8">
     <title>CV en ligne</title>
@@ -27,6 +55,7 @@
 <main>
     <div class="title title-last-name"><?= $data['LAST_NAME']; ?></div>
     <div class="title title-first-name"><?= $data['FIRST_NAME']; ?></div>
+    <div class="title"><?= $sectionName ?></div>
     <div class="description">
         <p><?= $year; ?> ans // <?= $data['PHONE_NUMBER']; ?> // <?= $data['MAIL']; ?> //</p>
         <p><?= $data['ADDRESS']; ?></p>
