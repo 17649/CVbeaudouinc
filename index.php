d9515fb5d91f5f680eef4d5fa766111cf70ebba9
Beaudouin Camille <beaudouin.camille@outlook.fr>
2019-04-16 12:05:49 +0200

    Première partie

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
         die;
     }
 
+    //var_dump($data);
+
+    //Todo: le mettre dans le fichier .ini
     $menu = [
         [urlencode("expériences"), "Expériences", "Mes expériences"],
         [urlencode("compétences"), "Compétences", "Mes compétences"],
@@ -32,6 +35,7 @@
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
+<div id="background"></div>
 <header>
     <nav>
         <?php foreach ($menu as $m): ?>
@@ -47,6 +51,63 @@
         <p><?= $data['ADDRESS']; ?></p>
     </div>
     <div class="section-title"><?= $sectionName ?></div>
+  <div class="container">
+    <div class="container-col">
+      <section>
+        <div class="title-group">
+          <img src="assets/round_language_white_18dp.png">
+          <span>Compétences</span>
+        </div>
+        <?php foreach ($data["SKILLS"] as $s): ?>
+        <?php $s = explode(",", $s);?>
+          <div class="skill-container">
+            <span class="skill-name"><?= $s[0] ?></span>
+            <div class="skill-dot-container">
+              <?php for($k = $s[1]; $k > 0; $k -= 1): ?>
+                <span class="skill-dot skill-dot-completed"></span>
+              <?php endfor; ?>
+              <?php for($k = 5 - $s[1]; $k > 0; $k -= 1): ?>
+                <span class="skill-dot"></span>
+              <?php endfor; ?>
+            </div>
+          </div>
+        <?php endforeach; ?>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non vulputate lorem. Nam et nisl vel risus posuere efficitur. Etiam lacus nulla, bibendum vel diam eu, tempus commodo ipsum. Praesent eros enim, euismod vitae risus sit amet, molestie vehicula odio. Nullam rhoncus nisi vitae tellus mollis, rutrum gravida arcu auctor. Sed gravida purus id sapien vehicula, eu mattis erat pellentesque. Cras pharetra eros vel nulla mollis, vitae commodo nibh porttitor. Nulla ac libero posuere, pretium diam sit amet, convallis tellus. Vestibulum interdum, velit vitae eleifend pretium, nibh urna pulvinar nunc, eu viverra nulla ligula at est. Sed vitae euismod lacus, ac sollicitudin purus. Nunc a finibus arcu, vel scelerisque ex. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non vulputate lorem. Nam et nisl vel risus posuere efficitur. Etiam lacus nulla, bibendum vel diam eu, tempus commodo ipsum. Praesent eros enim, euismod vitae risus sit amet, molestie vehicula odio. Nullam rhoncus nisi vitae tellus mollis, rutrum gravida arcu auctor. Sed gravida purus id sapien vehicula, eu mattis erat pellentesque. Cras pharetra eros vel nulla mollis, vitae commodo nibh porttitor. Nulla ac libero posuere, pretium diam sit amet, convallis tellus. Vestibulum interdum, velit vitae eleifend pretium, nibh urna pulvinar nunc, eu viverra nulla ligula at est. Sed vitae euismod lacus, ac sollicitudin purus. Nunc a finibus arcu, vel scelerisque ex. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non vulputate lorem. Nam et nisl vel risus posuere efficitur. Etiam lacus nulla, bibendum vel diam eu, tempus commodo ipsum. Praesent eros enim, euismod vitae risus sit amet, molestie vehicula odio. Nullam rhoncus nisi vitae tellus mollis, rutrum gravida arcu auctor. Sed gravida purus id sapien vehicula, eu mattis erat pellentesque. Cras pharetra eros vel nulla mollis, vitae commodo nibh porttitor. Nulla ac libero posuere, pretium diam sit amet, convallis tellus. Vestibulum interdum, velit vitae eleifend pretium, nibh urna pulvinar nunc, eu viverra nulla ligula at est. Sed vitae euismod lacus, ac sollicitudin purus. Nunc a finibus arcu, vel scelerisque ex. </p>
+      </section>
+      <section>
+        <div class="title-group">
+          <img src="assets/round_language_white_18dp.png">
+          <span>Langues parlées</span>
+        </div>
+        <img src="assets/flag_fr.svg">
+      </section>
+    </div>
+    <div class="container-col">
+      <section>
+        <div class="title-group">
+          <img src="assets/round_language_white_18dp.png">
+          <span>Formations</span>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non vulputate lorem. Nam et nisl vel risus posuere efficitur. Etiam lacus nulla, bibendum vel diam eu, tempus commodo ipsum. Praesent eros enim, euismod vitae risus sit amet, molestie vehicula odio. Nullam rhoncus nisi vitae tellus mollis, rutrum gravida arcu auctor. Sed gravida purus id sapien vehicula, eu mattis erat pellentesque. Cras pharetra eros vel nulla mollis, vitae commodo nibh porttitor. Nulla ac libero posuere, pretium diam sit amet, convallis tellus. Vestibulum interdum, velit vitae eleifend pretium, nibh urna pulvinar nunc, eu viverra nulla ligula at est. Sed vitae euismod lacus, ac sollicitudin purus. Nunc a finibus arcu, vel scelerisque ex. </p>
+      </section>
+      <section>
+        <div class="title-group">
+          <img src="assets/round_language_white_18dp.png">
+          <span>Expériences</span>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non vulputate lorem. Nam et nisl vel risus posuere efficitur. Etiam lacus nulla, bibendum vel diam eu, tempus commodo ipsum. Praesent eros enim, euismod vitae risus sit amet, molestie vehicula odio. Nullam rhoncus nisi vitae tellus mollis, rutrum gravida arcu auctor. Sed gravida purus id sapien vehicula, eu mattis erat pellentesque. Cras pharetra eros vel nulla mollis, vitae commodo nibh porttitor. Nulla ac libero posuere, pretium diam sit amet, convallis tellus. Vestibulum interdum, velit vitae eleifend pretium, nibh urna pulvinar nunc, eu viverra nulla ligula at est. Sed vitae euismod lacus, ac sollicitudin purus. Nunc a finibus arcu, vel scelerisque ex. </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non vulputate lorem. Nam et nisl vel risus posuere efficitur. Etiam lacus nulla, bibendum vel diam eu, tempus commodo ipsum. Praesent eros enim, euismod vitae risus sit amet, molestie vehicula odio. Nullam rhoncus nisi vitae tellus mollis, rutrum gravida arcu auctor. Sed gravida purus id sapien vehicula, eu mattis erat pellentesque. Cras pharetra eros vel nulla mollis, vitae commodo nibh porttitor. Nulla ac libero posuere, pretium diam sit amet, convallis tellus. Vestibulum interdum, velit vitae eleifend pretium, nibh urna pulvinar nunc, eu viverra nulla ligula at est. Sed vitae euismod lacus, ac sollicitudin purus. Nunc a finibus arcu, vel scelerisque ex. </p>
+      </section>
+      <section>
+        <div class="title-group">
+          <img src="assets/round_language_white_18dp.png">
+          <span>Hobbies et intérêt</span>
+        </div>
+      </section>
+    </div>
+  </div>
 </main>
 </body>
 </html>
